82564426ef7cf59f90ca47b327041ea200acc4fb
ermineaweb <ermineaweb@gmail.com>
2021-05-15 21:33:48 +0200

    .

--- sql/esx__all.sql
+++ sql/esx__all.sql
@@ -710,3 +710,114 @@ INSERT INTO `properties` VALUES
   (71, 'MBWPowerBrokerConvservative', 'MBW Power Broker Convservative', NULL, '{\"x\":-1392.74,\"y\":-480.18,\"z\":71.14}', '{\"x\":-1389.43,\"y\":-479.01,\"z\":71.14}', NULL, '[\"ex_sm_15_office_03b\"]', 'MazeBankWest', 0, 1, 0, '{\"x\":-1390.76,\"y\":-479.22,\"z\":72.04}', 2700000),
   (72, 'MBWPowerBrokerPolished', 'MBW Power Broker Polished', NULL, '{\"x\":-1392.74,\"y\":-480.18,\"z\":71.14}', '{\"x\":-1389.43,\"y\":-479.01,\"z\":71.14}', NULL, '[\"ex_sm_15_office_03c\"]', 'MazeBankWest', 0, 1, 0, '{\"x\":-1390.76,\"y\":-479.22,\"z\":72.04}', 2700000);
 ;
+
+-- PHONE
+USE `es_extended`;
+CREATE TABLE `user_contacts` (
+	`id` INT(11) NOT NULL AUTO_INCREMENT,
+	`identifier` VARCHAR(22) NOT NULL,
+	`name` VARCHAR(100) NOT NULL,
+	`number` INT(11) NOT NULL,
+	PRIMARY KEY (`id`),
+	INDEX `index_user_contacts_identifier_name_number` (`identifier`, `name`, `number`)
+);
+ALTER TABLE `users`
+	ADD COLUMN `phone_number` INT(11) NULL,
+	ADD UNIQUE INDEX `index_users_phone_number` (`phone_number`);
+
+-- IDENTITY
+USE `es_extended`;
+ALTER TABLE `users`
+	ADD COLUMN `firstname` VARCHAR(16) NULL DEFAULT NULL,
+	ADD COLUMN `lastname` VARCHAR(16) NULL DEFAULT NULL,
+	ADD COLUMN `dateofbirth` VARCHAR(10) NULL DEFAULT NULL,
+	ADD COLUMN `sex` VARCHAR(1) NULL DEFAULT NULL,
+	ADD COLUMN `height` INT NULL DEFAULT NULL
+;
+
+-- STATUS
+USE `es_extended`;
+ALTER TABLE `users` ADD COLUMN `status` LONGTEXT NULL;
+
+-- POLICE
+USE `es_extended`;
+INSERT INTO `addon_account` (name, label, shared) VALUES
+	('society_police', 'Police', 1)
+;
+INSERT INTO `datastore` (name, label, shared) VALUES
+	('society_police', 'Police', 1)
+;
+INSERT INTO `addon_inventory` (name, label, shared) VALUES
+	('society_police', 'Police', 1)
+;
+INSERT INTO `jobs` (name, label) VALUES
+	('police', 'LSPD')
+;
+INSERT INTO `job_grades` (job_name, grade, name, label, salary, skin_male, skin_female) VALUES
+	('police',0,'recruit','Recrue',20,'{}','{}'),
+	('police',1,'officer','Officier',40,'{}','{}'),
+	('police',2,'sergeant','Sergent',60,'{}','{}'),
+	('police',3,'lieutenant','Lieutenant',85,'{}','{}'),
+	('police',4,'boss','Commandant',100,'{}','{}')
+;
+CREATE TABLE `fine_types` (
+	`id` int NOT NULL AUTO_INCREMENT,
+	`label` varchar(255) DEFAULT NULL,
+	`amount` int DEFAULT NULL,
+	`category` int DEFAULT NULL,
+	PRIMARY KEY (`id`)
+);
+INSERT INTO `fine_types` (label, amount, category) VALUES
+	('Usage abusif du klaxon',30,0),
+	('Franchir une ligne continue',40,0),
+	('Circulation à contresens',250,0),
+	('Demi-tour non autorisé',250,0),
+	('Circulation hors-route',170,0),
+	('Non-respect des distances de sécurité',30,0),
+	('Arrêt dangereux / interdit',150,0),
+	('Stationnement gênant / interdit',70,0),
+	('Non respect  de la priorité à droite',70,0),
+	('Non-respect à un véhicule prioritaire',90,0),
+	('Non-respect d\'un stop',105,0),
+	('Non-respect d\'un feu rouge',130,0),
+	('Dépassement dangereux',100,0),
+	('Véhicule non en état',100,0),
+	('Conduite sans permis',1500,0),
+	('Délit de fuite',800,0),
+	('Excès de vitesse < 5 kmh',90,0),
+	('Excès de vitesse 5-15 kmh',120,0),
+	('Excès de vitesse 15-30 kmh',180,0),
+	('Excès de vitesse > 30 kmh',300,0),
+	('Entrave de la circulation',110,1),
+	('Dégradation de la voie publique',90,1),
+	('Trouble à l\'ordre publique',90,1),
+	('Entrave opération de police',130,1),
+	('Insulte envers / entre civils',75,1),
+	('Outrage à agent de police',110,1),
+	('Menace verbale ou intimidation envers civil',90,1),
+	('Menace verbale ou intimidation envers policier',150,1),
+	('Manifestation illégale',250,1),
+	('Tentative de corruption',1500,1),
+	('Arme blanche sortie en ville',120,2),
+	('Arme léthale sortie en ville',300,2),
+	('Port d\'arme non autorisé (défaut de license)',600,2),
+	('Port d\'arme illégal',700,2),
+	('Pris en flag lockpick',300,2),
+	('Vol de voiture',1800,2),
+	('Vente de drogue',1500,2),
+	('Fabriquation de drogue',1500,2),
+	('Possession de drogue',650,2),
+	('Prise d\'ôtage civil',1500,2),
+	('Prise d\'ôtage agent de l\'état',2000,2),
+	('Braquage particulier',650,2),
+	('Braquage magasin',650,2),
+	('Braquage de banque',1500,2),
+	('Tir sur civil',2000,3),
+	('Tir sur agent de l\'état',2500,3),
+	('Tentative de meurtre sur civil',3000,3),
+	('Tentative de meurtre sur agent de l\'état',5000,3),
+	('Meurtre sur civil',10000,3),
+	('Meurte sur agent de l\'état',30000,3),
+	('Meurtre involontaire',1800,3),
+	('Escroquerie à l\'entreprise',2000,2)
+;
