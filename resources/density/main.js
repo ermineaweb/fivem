600bdbc38e7251533ee573e580bf96e846f790ab
ermineaweb <ermineaweb@gmail.com>
2021-05-15 12:03:48 +0200

    .

--- resources/density/main.js
+++ resources/density/main.js
@@ -1,12 +1,10 @@
-const config = {
-    pedFrequency = 0.0,
-    trafficFrequency = 0.0,
-};
+const pedFrequency = 0.0;
+const trafficFrequency = 0.0;
 
-SetPedDensityMultiplierThisFrame(config.pedFrequency);
-SetScenarioPedDensityMultiplierThisFrame(config.pedFrequency, config.pedFrequency);
-SetRandomVehicleDensityMultiplierThisFrame(config.trafficFrequency) ;
-SetParkedVehicleDensityMultiplierThisFrame(config.trafficFrequency) ;
-SetVehicleDensityMultiplierThisFrame(config.trafficFrequency) ;
-       
-
+setImmediate(() => {
+  SetPedDensityMultiplierThisFrame(pedFrequency);
+  SetScenarioPedDensityMultiplierThisFrame(pedFrequency, pedFrequency);
+  SetRandomVehicleDensityMultiplierThisFrame(trafficFrequency);
+  SetParkedVehicleDensityMultiplierThisFrame(trafficFrequency);
+  SetVehicleDensityMultiplierThisFrame(trafficFrequency);
+});
