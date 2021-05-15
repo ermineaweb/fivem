a37a6721385e856501bb97c9007c23b6f8ae9124
ermineaweb <ermineaweb@gmail.com>
2021-05-15 18:47:45 +0200

    .

---
+++ sql/esx__all.sql
@@ -0,0 +1,112 @@
+USE `es_extended`;
+
+CREATE TABLE `users` (
+	`identifier` VARCHAR(60) NOT NULL,
+	`license` VARCHAR(60) DEFAULT NULL,
+	`accounts` LONGTEXT NULL DEFAULT NULL,
+	`group` VARCHAR(50) NULL DEFAULT 'user',
+	`inventory` LONGTEXT NULL DEFAULT NULL,
+	`job` VARCHAR(20) NULL DEFAULT 'unemployed',
+	`job_grade` INT NULL DEFAULT 0,
+	`loadout` LONGTEXT NULL DEFAULT NULL,
+	`position` VARCHAR(255) NULL DEFAULT '{"x":-269.4,"y":-955.3,"z":31.2,"heading":205.8}',
+
+	PRIMARY KEY (`identifier`)
+);
+
+CREATE TABLE `items` (
+	`name` VARCHAR(50) NOT NULL,
+	`label` VARCHAR(50) NOT NULL,
+	`weight` INT NOT NULL DEFAULT 1,
+	`limit` INT NOT NULL DEFAULT 10,	
+	`rare` TINYINT NOT NULL DEFAULT 0,
+	`can_remove` TINYINT NOT NULL DEFAULT 1,
+
+	PRIMARY KEY (`name`)
+);
+
+CREATE TABLE `job_grades` (
+	`id` INT NOT NULL AUTO_INCREMENT,
+	`job_name` VARCHAR(50) DEFAULT NULL,
+	`grade` INT NOT NULL,
+	`name` VARCHAR(50) NOT NULL,
+	`label` VARCHAR(50) NOT NULL,
+	`salary` INT NOT NULL,
+	`skin_male` LONGTEXT NOT NULL,
+	`skin_female` LONGTEXT NOT NULL,
+
+	PRIMARY KEY (`id`)
+);
+
+INSERT INTO `job_grades` VALUES (1,'unemployed',0,'unemployed','Unemployed', 50,'{}','{}');
+
+CREATE TABLE `jobs` (
+	`name` VARCHAR(50) NOT NULL,
+	`label` VARCHAR(50) DEFAULT NULL,
+
+	PRIMARY KEY (`name`)
+);
+
+INSERT INTO `jobs` VALUES ('unemployed','Unemployed');
+
+
+
+USE `es_extended`;
+
+ALTER TABLE `users` ADD COLUMN `skin` LONGTEXT NULL DEFAULT NULL;
+
+
+
+USE `es_extended`;
+
+INSERT INTO `addon_account` (name, label, shared) VALUES
+	('caution', 'Caution', 0)
+;
+
+INSERT INTO `jobs` (name, label) VALUES
+	('slaughterer', 'Abatteur'),
+	('fisherman', 'Pêcheur'),
+	('miner', 'Mineur'),
+	('lumberjack', 'Bûcheron'),
+	('fueler', 'Raffineur'),
+	('reporter', 'Journaliste'),
+	('tailor', 'Couturier')
+;
+
+INSERT INTO `job_grades` (job_name, grade, name, label, salary, skin_male, skin_female) VALUES
+	('lumberjack', 0, 'employee', 'Intérimaire', 0, '{}', '{}'),
+	('fisherman', 0, 'employee', 'Intérimaire', 0, '{}', '{}'),
+	('fueler', 0, 'employee', 'Intérimaire', 0, '{}', '{}'),
+	('reporter', 0, 'employee', 'Intérimaire', 0, '{}', '{}'),
+	('tailor',0,'employee','Intérimaire',0,'{\"mask_1\":0,\"arms\":1,\"glasses_1\":0,\"hair_color_2\":4,\"makeup_1\":0,\"face\":19,\"glasses\":0,\"mask_2\":0,\"makeup_3\":0,\"skin\":29,\"helmet_2\":0,\"lipstick_4\":0,\"sex\":0,\"torso_1\":24,\"makeup_2\":0,\"bags_2\":0,\"chain_2\":0,\"ears_1\":-1,\"bags_1\":0,\"bproof_1\":0,\"shoes_2\":0,\"lipstick_2\":0,\"chain_1\":0,\"tshirt_1\":0,\"eyebrows_3\":0,\"pants_2\":0,\"beard_4\":0,\"torso_2\":0,\"beard_2\":6,\"ears_2\":0,\"hair_2\":0,\"shoes_1\":36,\"tshirt_2\":0,\"beard_3\":0,\"hair_1\":2,\"hair_color_1\":0,\"pants_1\":48,\"helmet_1\":-1,\"bproof_2\":0,\"eyebrows_4\":0,\"eyebrows_2\":0,\"decals_1\":0,\"age_2\":0,\"beard_1\":5,\"shoes\":10,\"lipstick_1\":0,\"eyebrows_1\":0,\"glasses_2\":0,\"makeup_4\":0,\"decals_2\":0,\"lipstick_3\":0,\"age_1\":0}','{\"mask_1\":0,\"arms\":5,\"glasses_1\":5,\"hair_color_2\":4,\"makeup_1\":0,\"face\":19,\"glasses\":0,\"mask_2\":0,\"makeup_3\":0,\"skin\":29,\"helmet_2\":0,\"lipstick_4\":0,\"sex\":1,\"torso_1\":52,\"makeup_2\":0,\"bags_2\":0,\"chain_2\":0,\"ears_1\":-1,\"bags_1\":0,\"bproof_1\":0,\"shoes_2\":1,\"lipstick_2\":0,\"chain_1\":0,\"tshirt_1\":23,\"eyebrows_3\":0,\"pants_2\":0,\"beard_4\":0,\"torso_2\":0,\"beard_2\":6,\"ears_2\":0,\"hair_2\":0,\"shoes_1\":42,\"tshirt_2\":4,\"beard_3\":0,\"hair_1\":2,\"hair_color_1\":0,\"pants_1\":36,\"helmet_1\":-1,\"bproof_2\":0,\"eyebrows_4\":0,\"eyebrows_2\":0,\"decals_1\":0,\"age_2\":0,\"beard_1\":5,\"shoes\":10,\"lipstick_1\":0,\"eyebrows_1\":0,\"glasses_2\":0,\"makeup_4\":0,\"decals_2\":0,\"lipstick_3\":0,\"age_1\":0}'),
+	('miner', 0, 'employee', 'Intérimaire', 0, '{"tshirt_2":1,"ears_1":8,"glasses_1":15,"torso_2":0,"ears_2":2,"glasses_2":3,"shoes_2":1,"pants_1":75,"shoes_1":51,"bags_1":0,"helmet_2":0,"pants_2":7,"torso_1":71,"tshirt_1":59,"arms":2,"bags_2":0,"helmet_1":0}', '{}'),
+	('slaughterer',0,'employee','Intérimaire',0,'{\"age_1\":0,\"glasses_2\":0,\"beard_1\":5,\"decals_2\":0,\"beard_4\":0,\"shoes_2\":0,\"tshirt_2\":0,\"lipstick_2\":0,\"hair_2\":0,\"arms\":67,\"pants_1\":36,\"skin\":29,\"eyebrows_2\":0,\"shoes\":10,\"helmet_1\":-1,\"lipstick_1\":0,\"helmet_2\":0,\"hair_color_1\":0,\"glasses\":0,\"makeup_4\":0,\"makeup_1\":0,\"hair_1\":2,\"bproof_1\":0,\"bags_1\":0,\"mask_1\":0,\"lipstick_3\":0,\"chain_1\":0,\"eyebrows_4\":0,\"sex\":0,\"torso_1\":56,\"beard_2\":6,\"shoes_1\":12,\"decals_1\":0,\"face\":19,\"lipstick_4\":0,\"tshirt_1\":15,\"mask_2\":0,\"age_2\":0,\"eyebrows_3\":0,\"chain_2\":0,\"glasses_1\":0,\"ears_1\":-1,\"bags_2\":0,\"ears_2\":0,\"torso_2\":0,\"bproof_2\":0,\"makeup_2\":0,\"eyebrows_1\":0,\"makeup_3\":0,\"pants_2\":0,\"beard_3\":0,\"hair_color_2\":4}','{\"age_1\":0,\"glasses_2\":0,\"beard_1\":5,\"decals_2\":0,\"beard_4\":0,\"shoes_2\":0,\"tshirt_2\":0,\"lipstick_2\":0,\"hair_2\":0,\"arms\":72,\"pants_1\":45,\"skin\":29,\"eyebrows_2\":0,\"shoes\":10,\"helmet_1\":-1,\"lipstick_1\":0,\"helmet_2\":0,\"hair_color_1\":0,\"glasses\":0,\"makeup_4\":0,\"makeup_1\":0,\"hair_1\":2,\"bproof_1\":0,\"bags_1\":0,\"mask_1\":0,\"lipstick_3\":0,\"chain_1\":0,\"eyebrows_4\":0,\"sex\":1,\"torso_1\":49,\"beard_2\":6,\"shoes_1\":24,\"decals_1\":0,\"face\":19,\"lipstick_4\":0,\"tshirt_1\":9,\"mask_2\":0,\"age_2\":0,\"eyebrows_3\":0,\"chain_2\":0,\"glasses_1\":5,\"ears_1\":-1,\"bags_2\":0,\"ears_2\":0,\"torso_2\":0,\"bproof_2\":0,\"makeup_2\":0,\"eyebrows_1\":0,\"makeup_3\":0,\"pants_2\":0,\"beard_3\":0,\"hair_color_2\":4}')
+;
+
+INSERT INTO `items` (`name`, `label`, `weight`) VALUES
+	('alive_chicken', 'Poulet vivant', 1),
+	('slaughtered_chicken', 'Poulet abattu', 1),
+	('packaged_chicken', 'Poulet en barquette', 1),
+	('fish', 'Poisson', 1),
+	('stone', 'Pierre', 1),
+	('washed_stone', 'Pierre Lavée', 1),
+	('copper', 'Cuivre', 1),
+	('iron', 'Fer', 1),
+	('gold', 'Or', 1),
+	('diamond', 'Diamant', 1),
+	('wood', 'Bois', 1),
+	('cutted_wood', 'Bois coupé', 1),
+	('packaged_plank', 'Paquet de planches', 1),
+	('petrol', 'Pétrole', 1),
+	('petrol_raffin', 'Pétrole Raffiné', 1),
+	('essence', 'Essence', 1),
+	('wool', 'Laine', 1),
+	('fabric', 'Tissu', 1),
+	('clothe', 'Vêtement', 1)
+;
+
+
+
+USE `es_extended`;
+
+ALTER TABLE jobs add whitelisted BOOLEAN NOT NULL DEFAULT FALSE;
