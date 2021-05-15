e1d6098f905ec7d380ef791f925634f12908305a
ermineaweb <ermineaweb@gmail.com>
2021-05-15 18:24:20 +0200

    .

--- resources/tp/fxmanifest.lua
+++
@@ -1,8 +0,0 @@
-version '1.0.0'
-author 'ro'
-description 'test'
-fx_version 'cerulean'
-
-client_script 'main.js'
-
-games { 'gta5' }
--- resources/tp/main.js
+++
@@ -1,6 +0,0 @@
-RegisterCommand("tp", tp, false);
-
-function tp() {
-  SetEntityCoords(PlayerPedId(), -1377.514282266, -2852.64941406, 13.9448, true, true, true, false);
-  SetEntityHeading(PlayerPedId(), 360);
-}
