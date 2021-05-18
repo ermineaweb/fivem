4fcd52982efd50184c7425092e12613a151b322d
ermineaweb <ermineaweb@gmail.com>
2021-05-18 13:42:05 +0200

    .

--- sql/esx__all.sql
+++ sql/esx__all.sql
@@ -1047,3 +1047,17 @@ USE `es_extended`;
 INSERT INTO `items` (name, label, `weight`) VALUES
 	('jewels', 'Jewels', 1)
 ;
+
+-- REAL PARKING
+USE `es_extended`;
+CREATE TABLE `car_parking`  (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `owner` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `plate` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `data` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL,
+  `time` bigint(20) NOT NULL,
+  `parking` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
+
+INSERT INTO `items`(`name`, `label`, `limit`) VALUES ('parkingcard', 'Parking Card', -1);
