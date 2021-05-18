1ffa787222ae0925f365e3eb11d6761e1ac052cc
ermineaweb <ermineaweb@gmail.com>
2021-05-18 13:36:55 +0200

    .

---
+++ resources/chairbed/fxmanifest.lua
@@ -0,0 +1,15 @@
+version '1.0.0'
+author 'ro'
+description 'chair bed sit'
+fx_version 'cerulean'
+
+client_scripts{
+	'client.lua',
+	'config.lua'
+}
+
+server_scripts{
+	'server.lua'
+}
+
+games { 'gta5' }
---
+++ resources/realistic_vehicle_failure/fxmanifest.lua
@@ -0,0 +1,16 @@
+version '1.0.0'
+author 'ro'
+description 'density traffic'
+fx_version 'cerulean'
+
+client_scripts {
+	"config.lua",
+	"client.lua"
+}
+
+server_scripts {
+	"config.lua",
+	"server.lua"
+}
+
+games { 'gta5' }
