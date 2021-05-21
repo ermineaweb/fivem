47dfd931a721b9cff8b04cd42ba81e4b2e731078
ermineaweb <ermineaweb@gmail.com>
2021-05-21 17:51:36 +0200

    .

--- sql/esx__all.sql
+++ sql/esx__all.sql
@@ -887,12 +887,15 @@ INSERT INTO `items` (name, label, weight) VALUES
 
 -- DRUGS
 USE `es_extended`;
-INSERT INTO `items` (`name`, `label`, `weight`, `rare`, `can_remove`) VALUES
-	('cannabis', 'Cannabis', 3, 0, 1),
-	('marijuana', 'Marijuana', 2, 0, 1)
-;
-INSERT INTO `licenses` (`type`, `label`) VALUES
-	('weed_processing', 'Weed Processing License')
+INSERT INTO `items` (`name`, `label`, `weight`, `limit`, `rare`, `can_remove`) VALUES
+	('weed', 'Weed', 1, 200, 0, 1),
+	('weed_pooch', 'Sachet de Weed', 1, 50, 0, 1),
+	('meth', 'Meth', 1, 150, 0, 1),
+	('meth_pooch', 'Sachet de Meth', 1, 25, 0, 1),
+	('coke', 'Cocaine', 1, 150, 0, 1),
+	('coke_pooch', 'Sachet de Cocaine', 1, 25, 0, 1),
+	('opium', 'Opium', 1, 150, 0, 1),
+	('opium_pooch', 'Sachet d\'Opium', 1, 25, 0, 1)
 ;
 
 -- AMBULANCES
