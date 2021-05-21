607a231f7f2167f8080331785b2c68fe31f0e261
ermineaweb <ermineaweb@gmail.com>
2021-05-21 11:24:44 +0200

    .

--- resources/[esx]/esx_weed/esx_drugs.sql
+++ resources/[esx]/esx_weed/esx_drugs.sql
@@ -1,10 +1,18 @@
 USE `es_extended`;
 
 INSERT INTO `items` (`name`, `label`, `weight`, `limit`, `rare`, `can_remove`) VALUES
-	('weed', 'Plan de weed', 1, 200, 0, 1),
-	('weed_pooch', 'Sachet de weed', 1, 50, 0, 1),
+	('weed', 'Weed', 1, 200, 0, 1),
+	('weed_pooch', 'Sachet de Weed', 1, 50, 0, 1),
+	('meth', 'Meth', 1, 150, 0, 1),
+	('meth_pooch', 'Sachet de Meth', 1, 25, 0, 1),
+	('coke', 'Cocaine', 1, 150, 0, 1),
+	('coke_pooch', 'Sachet de Cocaine', 1, 25, 0, 1),
+	('opium', 'Opium', 1, 150, 0, 1),
+	('opium_pooch', 'Sachet d\'Opium', 1, 25, 0, 1)
 ;
 
 INSERT INTO `licenses` (`type`, `label`) VALUES
 	('weed_processing', 'Licence de transformation de la weed')
 ;
+
+
