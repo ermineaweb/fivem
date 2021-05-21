6fc81400d78d52cc210104b82808af04d0674608
ermineaweb <ermineaweb@gmail.com>
2021-05-21 11:54:04 +0200

    .

---
+++ resources/[esx]/[drugs]/esx_weed/locales/fr.lua
@@ -0,0 +1,30 @@
+Locales ['fr'] = {
+  -- weed
+  ['weed_pickupprompt'] = 'appuyez sur ~INPUT_CONTEXT~ pour récolter un plan de ~g~Weed~s~.',
+  ['weed_inventoryfull'] = 'vous n\'avez plus assez de place dans votre inventaire pour récolter de la ~g~Weed~s~.',
+  ['weed_processprompt'] = 'appuyez ~INPUT_CONTEXT~ pour démarrer la ~g~tranformation de la Weed~s~.',
+  ['weed_processingstarted'] = 'transformation de la ~g~Weed~s~ en ~g~Pochon de Weed~s~...',
+  ['weed_processingfull'] = 'transformation ~r~annulée~s~. Votre inventaire est plein!',
+  ['weed_processingenough'] = 'vous devez avoir ~b~5x~s~ ~g~Weed~s~ pour lancer une transformation.',
+  ['weed_processed'] = 'vous avez transformé ~b~5x~s~ ~g~Weed~s~ en ~b~1x~s~ ~g~Pochon de Weed~s~',
+  ['weed_processingtoofar'] = 'la transformation a été ~r~annulée~s~. Vous êtes sorti de la zone.',
+
+  -- drug dealer
+  ['dealer_prompt'] = 'appuyez sur ~INPUT_CONTEXT~ pour parler avec le ~r~Drug Dealer~s~.',
+  ['dealer_title'] = 'drug Dealer',
+  ['dealer_item'] = '$%s',
+  ['dealer_notenough'] = 't\'en as pas assez à vendre!',
+  ['dealer_sold'] = 'vous avez vendu ~b~%sx~s~ ~y~%s~s~ pour ~g~$%s~s~',
+
+  -- license
+  ['license_title'] = 'vous devez posséder une license spécifique vous autorisant à transformer ce produit. Vous pouvez l\'acheter ici.',
+  ['license_no'] = 'non',
+  ['license_bought'] = 'vous avez acheté ~b~%s~s~ pour ~r~$%s~s~',
+  ['license_bought_fail'] = 'vous ne pouvez pas acheter ~b~%s~s~!',
+  ['license_weed'] = 'permis de transformation de weed',
+
+  -- blips
+  ['blip_weedfield'] = 'champs de weed',
+  ['blip_weedprocessing'] = 'transformation de weed',
+  ['blip_drugdealer'] = 'drug Dealer',
+}
