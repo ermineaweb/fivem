cebccc18d55ce67188ca83725d8adec26f2ed068
ermineaweb <ermineaweb@gmail.com>
2021-05-26 19:11:28 +0200

    change volume mongo

--- resources/esx_GangScript/config.lua
+++ resources/esx_GangScript/config.lua
@@ -11,7 +11,7 @@ Config.Gangs = {
 		JobName = "ballas", -- need to be same as in SQL
 		-- if you want blip on the map, just simply fill fields below. You can delete this section if neccessary.
 		Blip = { 
-		  Pos     = { x = 425.130, y = -979.558, z = 30.711 },
+		  Pos     = { x = -1119.31, y = -1606.65, z = 4.4 },
 		  Sprite  = 59,
 		  Display = 4,
 		  Scale   = 1.2,
@@ -20,24 +20,23 @@ Config.Gangs = {
 		},		
 		EnablePlayerManagement = true,	-- if you want to enable boss player management
 		BossActions = { -- where this management should be?
-			{x = 426.2, y = -981.14, z = 29.71 },
+			{x = -1117.74, y = -1589.46, z = 4.58 },
 		},
 		AuthorizedToBossActionsRanks = { -- ranks from SQL to have access to player management
 			"rank4",
 		},
 		Vehicles = { -- gangs garage. You can delete this section, but remember to delete VehicleDeleters and Authorized Vehicles! You can add multiple garages.
 			{
-				Spawner    = { x = 428.130, y = -975.558, z = 30.711 },
-				SpawnPoint = { x = 418.130, y = -975.558, z = 30.711 },
-				Heading    = 331.74,
+				Spawner    = { x = -1128.06, y = -1614.31, z = 4.4 },
+				SpawnPoint = { x = -1118.7, y = -1608.41, z = 4.29 },
+				Heading    = 28.89,
 			}		
 		},
 		VehicleDeleters = { -- Same. You can add a lot vehicle deleters :)
-			{x = 91.03, y = -1964.94, z = 19.75},
-			{x = 411.51, y = -981.99, z = 29.42},
+			{x = -1127.48, y = -1607.92, z = 4.4},
 		},	
 		Armories = { -- same.. Just armories (places where you can put your weapon, and everyone from gang can see this.
-			{x = 422.130, y = -975.558, z = 30.711},
+			{x = -1117.42, y = -1626.24, z = 4.45},
 		},
 	},
 	--another gang, just simply fill as much as you want to :P 
